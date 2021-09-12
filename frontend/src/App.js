e9da396cc80d4b799273d4dd66db6889ac41c4bc
shivaniamborkar <shivaniamborkar6@gmail.com>
2021-09-12 12:40:54 +0530

    share code to github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,34 @@
 import React from "react";
-import data from "./data";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            <img
-              className="brand"
-              src="./images/Surabhi_Plywood.png"
-              alt="brand"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="/signin">Sign In</a>
-          <a href="/cart">
-            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-          </a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/products/${product._id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                />
+    <BrowserRouter>
+      <div>
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">
+                <img src="./images/Surabhi_Plywood.png" alt="Surabhi Plywood" />
               </a>
-              <div className="card-body">
-                <a href={`/products/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star-half-o"></i>
-                  </span>
-                </div>
-                <div className="price">
-                  <i className="fa fa-rupee"></i>
-                  {product.price}
-                </div>
-              </div>
             </div>
-          ))}
+            <div>
+              <a href="/signin">Sign In</a>
+              <a href="/cart">
+                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+              </a>
+            </div>
+          </header>
+          <main>
+            <Route path="/product/:id" component={ProductScreen} exact></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+          </main>
+          <footer className="row center">All rights reserved</footer>
         </div>
-      </main>
-      <footer className="row center">All rights reserved</footer>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
